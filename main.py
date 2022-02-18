0120813cad5527067c5b544578c6bd1e3d2b0f83
jsmsj <67999970+jsmsj@users.noreply.github.com>
2022-02-18 13:30:02 +0530

    add license

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+# This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Gclone-Discord-Utilities Bot 1.0.0 by jsmsj.
+# GClone made by Donwa on GitHub.
+# Inspired from REKULOUS' CloneCord V6 Beta [https://github.com/Rekulous/CloneCord-Bot]
+# Original Discord "GClone-Bot" by KushTheApplusser!
 """Imports"""
 import discord, os
 import traceback,sys
